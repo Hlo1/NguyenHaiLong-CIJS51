2d0b8c59f467feafa5586fb966d013cac3cb5b1c
Hlo1 <hailong211975@gmail.com>
2020-10-07 07:31:50 +0700

    second commit

--- Chatapp/js/index.js
+++ Chatapp/js/index.js
@@ -1,3 +1,16 @@
 window.onload=()=>{
-    view.setActiveScreen('loginScreen')
-} 
+    var firebaseConfig = {
+        apiKey: "AIzaSyBFoI_aHfRqwDQygOZINUacHqkxNUqmZ48",
+        authDomain: "chat-app-9deb8.firebaseapp.com",
+        databaseURL: "https://chat-app-9deb8.firebaseio.com",
+        projectId: "chat-app-9deb8",
+        storageBucket: "chat-app-9deb8.appspot.com",
+        messagingSenderId: "441922901862",
+        appId: "1:441922901862:web:8d08ca88da0bff59901aa7",
+        measurementId: "G-SXJGWL57JP"
+      };
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    console.log(firebase.app().name)
+    view.setActiveScreen('registerScreen')
+}
--- Chatapp/js/view.js
+++ Chatapp/js/view.js
@@ -8,11 +8,35 @@ view.setActiveScreen = (screenName)=>{
         case 'registerScreen':
         document.getElementById('app').innerHTML = components.registerPage
         document.getElementById('redirect-login').addEventListener('click',()=>{view.setActiveScreen('loginScreen')})
+        const registerForm = document.getElementById('register-form')
+        registerForm.addEventListener('submit',(event)=>{
+            event.preventDefault();
+            const dataRegister = {
+                firstName: registerForm.firstName.value,
+                lastName: registerForm.lastName.value,
+                email: registerForm.email.value,
+                password: registerForm.password.value,
+                confirmPassword: registerForm.confirmPassword.value
+            }
+            controller.register(dataRegister)
+        })
         break;
 
         case 'loginScreen':
         document.getElementById('app').innerHTML = components.loginPage
         document.getElementById('redirect-register').addEventListener('click',()=>{view.setActiveScreen('registerScreen')})
+        const loginForm = document.getElementById('login-form')
+        loginForm.addEventListener('submit',(event)=>{
+            event.preventDefault();
+            const dataLogin = {
+                email: loginForm.email.value,
+                password: loginForm.password.value,
+            }
+            controller.login(dataLogin)
+        })
         break;
     }
+}
+view.setErrorMessage = (elementId, message)=>{
+    document.getElementById(elementId).innerText = message;
 }
